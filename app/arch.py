239ae71dbf24750f9b13ff09bc1484f6194055c1
Asirwad <asirwadsali@gmail.com>
2023-07-24 16:21:46 +0530

    arch.py

--- app/arch.py
+++ app/arch.py
@@ -1,10 +1,16 @@
 from tensorflow.keras import Sequential, layers
 
 
-def get_model():
+def get_model(img_width, img_height):
     model = Sequential([
         # input is a 28x28 image then flatten the 28x28 into a single 784x1 layer
-        layers.Flatten(input_shape=(28, 28))
+        layers.Flatten(input_shape=(img_width, img_height)),
+
+        # hidden layer is 128 deep
+        layers.Dense(units=128, activation='relu'),
+
+        # output is 0-9 (depending on what piece of cloth it is). return maximum
+        layers.Dense(units=10, activation='softmax')
 
     ])
-get_model()
+    return model
